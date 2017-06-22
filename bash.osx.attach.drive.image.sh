b96570809519e77b182c1f8252ac0c27e79b149c
climateamante <Research Ranks>
2017-06-22 01:57:18 -0700

    tested and updated with printf

--- bash.osx.attach.drive.image.sh
+++ bash.osx.attach.drive.image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Attach drive image with password
+# Tested With 10.11.6
 # Last updated at 06-22-2017
 
 
@@ -7,6 +8,6 @@
 image_location=$(echo ~/documents/folder/image.sparsebundle)
 
 # Not Recommended, but you are able to pass a password in plain text
-# echo "${password}" | hdiutil attach "${image_location}"
+# printf '%s\0' "${password}" | hdiutil attach "${image_location}" -stdinpass
 
-hdiutil attach "${image_location}" -stdinpass
+hdiutil attach "${image_location}"
